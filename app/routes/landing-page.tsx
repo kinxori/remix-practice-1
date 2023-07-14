a23a7bc7ad7f62df40c2a288dd1804b29d577673
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-13 18:58:20 -0600

    adding loader function to receive data to the route

--- app/routes/landing-page.tsx
+++ app/routes/landing-page.tsx
@@ -1,3 +1,12 @@
+export const loader = async () => {
+  const data = await (await fetch("https://rickandmortyapi.com/api/character/")).json();
+  return null;
+};
+
 export default function LandingPage() {
-  return <div>Hola mundo</div>;
+  return (
+    <article>
+      <div>Hola mundo</div>
+    </article>
+  );
 }
