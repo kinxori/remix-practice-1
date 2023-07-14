8e8a28926179fe3f930852c9e0a0e5c82cc86ddb
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-13 19:15:12 -0600

    adding functions for useLoaderData and useActionData to pass data to client

--- app/routes/landing-page.tsx
+++ app/routes/landing-page.tsx
@@ -1,17 +1,33 @@
+import { useActionData, useLoaderData } from "@remix-run/react";
+
 export const loader = async () => {
   const data = await (await fetch("https://rickandmortyapi.com/api/character/")).json();
   return null;
 };
 
-export const action = async () => {
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  console.log("data ", formData.get("email"));
   return null;
 };
 
 export default function LandingPage() {
+  const dataLoader = useLoaderData();
+  const formDataAction = useActionData();
   return (
     <article>
       <div>Hola mundo</div>
-      <form></form>
+      <form method="post">
+        <label>
+          Email
+          <input type="email" name="email" placeholder="email"></input>
+        </label>
+        <label>
+          Password
+          <input type="text" name="password" placeholder="password"></input>
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
     </article>
   );
 }
