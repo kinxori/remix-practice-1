64ad51b9b70fc6f97a99d9abf567a5b96cc63578
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-13 19:46:03 -0600

    adding comments to route for better understanding

--- app/routes/landing-page.tsx
+++ app/routes/landing-page.tsx
@@ -1,16 +1,22 @@
 import { useActionData, useLoaderData } from "@remix-run/react";
 import styles from "~/styles/styles.css";
 
+// Loader to consume API
+
 export const loader = async () => {
   const data = await (await fetch("https://rickandmortyapi.com/api/character/")).json();
   return null;
 };
+
+// Action to recieve data from Form
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   console.log("data ", formData.get("email"));
   return null;
 };
+
+// Metas for route page
 
 export const meta = () => {
   return [
@@ -20,9 +26,13 @@ export const meta = () => {
   ];
 };
 
+// Links for route page
+
 export const links = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
+
+// Cliente component
 
 export default function LandingPage() {
   const dataLoader = useLoaderData();
