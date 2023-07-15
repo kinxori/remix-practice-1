6231df784020c270eb2ae19499942ce9e06a26cf
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-14 20:19:52 -0600

    this seed code is working

--- seed.js
+++ seed.js
@@ -15,13 +15,13 @@ const prisma = new PrismaClient();
 // Creating seed based on my schema.prisma
 
 const getProduct = () => {
-  const name = faker.commerce.productName();
-  const product = {
-    slug: faker.helpers.slugify(name),
-    title: name,
+  const title = faker.commerce.productName();
+  return {
+    slug: faker.helpers.slugify(title),
+    title: title,
     body: faker.commerce.productDescription(),
     price: faker.commerce.price(),
-    images: [
+    image: [
       faker.image.url(),
       faker.image.url(),
       faker.image.url(),
@@ -29,7 +29,6 @@ const getProduct = () => {
       faker.image.url(),
     ],
   };
-  return product;
 };
 
 //This has to be async to be descriptive and here we are
@@ -44,14 +43,14 @@ const saveInDB = async () => {
 // Creating an array to return an array of promises to the client
 // ----With Block
 
-const seed = async () => {
-  const promises = [...Array(100).keys()].map(saveInDB);
-  await Promise.all(promises);
-};
+// const seed = async () => {
+//   const promises = [...Array(100).keys()].map(saveInDB);
+//   await Promise.all(promises);
+// };
 
 // ----With 1 liner
 
-// const seed = async () => await Promise.all([...Array(10).keys()].map(saveInDB));
+const seed = async () => await Promise.all([...Array(200).keys()].map(saveInDB));
 
 //We callback seed function to invoke every promise
 
