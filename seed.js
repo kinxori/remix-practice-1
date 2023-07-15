149fcab246e94eb675087268599f524305270d83
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-14 19:50:21 -0600

    seed function file completed

--- seed.js
+++ seed.js
@@ -1,6 +1,10 @@
 import { faker } from "@faker-js/faker";
 import { PrismaClient } from "@prisma/client";
 
+// Creating new Prisma client for whole seed file
+
+const prisma = new PrismaClient();
+
 // Creating seed based on my schema.prisma
 
 const getProduct = () => {
@@ -15,6 +19,27 @@ const getProduct = () => {
   return product;
 };
 
-const saveInDB = () => {
-  return null;
+//This has to be async to be descriptive and here we are
+// sending the data model to prisma cliente and to mongo
+
+const saveInDB = async () => {
+  return prisma.product.createMany({
+    data: getProduct(),
+  });
 };
+
+// Creating an array to return an array of promises to the client
+// ----With Block
+
+const seed = async () => {
+  const promises = [...Array(10).keys()].map(saveInDB);
+  await Promise.all(promises);
+};
+
+// ----With 1 liner
+
+// const seed = async () => await Promise.all([...Array(10).keys()].map(saveInDB));
+
+//We callback seed function to invoke every promise
+
+seed();
