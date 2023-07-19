aa8fd73f3a80feaa548162e27e57a79f1d35dee7
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-18 19:37:28 -0600

    adding tailwind config file to repo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    screens: {
+      tablet: "601px",
+      desktop: "781px",
+    },
+  },
+  plugins: [],
+};
