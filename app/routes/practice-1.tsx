089cfda6bf799de96f120932bfff1d46f18f33dc
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-27 22:50:55 -0600

    updating content - route practice 1 - progress 2

--- app/routes/practice-1.tsx
+++ app/routes/practice-1.tsx
@@ -26,7 +26,7 @@ export const action = async ({ request }: any) => {
 export const meta = () => {
   return [
     {
-      title: "Practica 1",
+      title: "Practice 1",
     },
   ];
 };
@@ -48,19 +48,18 @@ export default function LandingPage() {
   const dataLoader = useLoaderData();
   const formDataAction = useActionData();
   return (
-    <article className="w-[80%] mx-auto my-10 ">
-      <div className="flex flex-col     ">
+    <article className="w-[80%] mx-auto my-10 text-white ">
+      <div className="flex flex-col ">
         <h1 className="font-bold text-[54px] font-[rubik] ">Hey there!</h1>
-        <h3 className="font-bold text-[16px] font-[rubik]   ">Practice 1</h3>
-        <Link to="/" className="ml-auto  ">
-          <button className=" px-4 py-1  bg-gray-200 rounded-[10px] my-3">Go Back</button>
+        <h3 className="font-bold text-[20px] font-[rubik]   ">Practice 1</h3>
+        <Link to="/" className="ml-auto">
+          <button className="px-4 py-1 bg-gray-200 text-black rounded-[10px] my-3">Go Back</button>
         </Link>
-        <p>In this page we can take a look to the basic structure of a Remix route.</p>
-
-        <p>There a 5 main functions to have a good Remix route.</p>
       </div>
-      <div>
-        <h3 className="font-bold text-[16px] font-[rubik]   ">The main component:</h3>
+      <div className="flex flex-col gap-3 my-5">
+        <p>In this page we can take a look to the basic structure of a Remix route.</p>
+        <p>There are 5 main parts to have a complete Remix route.</p>
+        <h3 className="font-bold text-[20px] font-[rubik] ">The main component:</h3>
         <p>
           Here we have the main component which is the function exported as default where display
           our JSX.
